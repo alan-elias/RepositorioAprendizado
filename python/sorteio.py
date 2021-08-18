203752a06f6ed17ec49255ffe033108bdc2e0a1b
Alan Elias <alaneliasn@gmail.com>
2021-08-18 01:28:22 -0300

    Ajustes no código de sorteio

--- python/sorteio.py
+++ python/sorteio.py
@@ -1,12 +1,12 @@
 from random import sample
 
-players = ['Alan', 'Anderson', 'Higor', 'Lucas' ,'Luiz', 'Natanael', 'Vitor'] #JOGADORES
+players = ['Insigne', 'Lukaku', 'Messi', 'Neymar' ,'Mbappe', 'Cristiano Ronaldo', 'Benzema'] #JOGADORES
 list(set(players)) #DEIXANDO IMUTAVEL
 pote1 = sample(players, 3) #ESCOLHENDO 3 PRA 1 TIME
 list(set(pote1)) #DEIXANDO IMUTAVEL
-pote2 = list(set(players)- set(pote1)) #RETIRANDO OS QUE NÃ‚O FORAM SORTEADOS PARA O TIME 1
+pote2 = list(set(players)- set(pote1)) #RETIRANDO OS QUE NÃO FORAM SORTEADOS PARA O TIME 1
 
-print(f'\033[1m{" JOGADORES ":=^45}\033[m')
-print(f'\033[1;33m  POTE1: {pote1} \033[m') #EXPORTANDO QUEM FOI SELECIONADO ANTES
-print(f'\033[1;34m  POTE2: {sample(pote2, 3)} \033[m') #SELECIONANDO E EXPORTANDO DE QUEM SOBROU
-print(f'\033[1m{"":=^45}\033[m')
+print(f'\033[1m{" JOGADORES ":=^50}\033[m')
+print(f'\033[1;33m  TIME 1: {pote1} \033[m') #EXPORTANDO QUEM FOI SELECIONADO ANTES
+print(f'\033[1;34m  TIME 2: {sample(pote2, 3)} \033[m') #SELECIONANDO E EXPORTANDO DE QUEM SOBROU
+print(f'\033[1m{"":=^50}\033[m')
